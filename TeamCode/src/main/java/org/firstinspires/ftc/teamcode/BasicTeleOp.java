fd936a34b6bf83edb74ab560920e249d565028fa
Dhruva Chayapathy <dhruva.chayapathy@gmail.com>
2022-06-22 15:09:05 -0400

    vignesh house

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -1,16 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.ConditionalCommand;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.StartEndCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.commands.BasicMecanumDriveCommand;
 import org.firstinspires.ftc.teamcode.subsystems.BasicMecanumDrive;
 
 @TeleOp
 public class BasicTeleOp extends CommandOpMode {
     private Motor fL,fR,bL,bR;
+    private SimpleServo servo;
+    private Motor intakeMotor;
     private BasicMecanumDrive drive;
     private BasicMecanumDriveCommand driveC;
     private GamepadEx controller;
@@ -21,6 +29,8 @@ public class BasicTeleOp extends CommandOpMode {
         fR=new Motor(hardwareMap,"frontRight");
         bR=new Motor(hardwareMap,"backRight");
         bL=new Motor(hardwareMap,"backLeft");
+        intakeMotor = new Motor(hardwareMap,"intakeMotor");
+        servo = new SimpleServo(hardwareMap,"servo",-180,180, AngleUnit.DEGREES);
         //set the running mode for motors
         fL.motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         fR.motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -37,5 +47,11 @@ public class BasicTeleOp extends CommandOpMode {
         //register the subsystem and set the command as the default for the drive subsystem
         register(drive);
         drive.setDefaultCommand(driveC);
+        controller.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whenHeld(new StartEndCommand(()->{intakeMotor.set(-0.5);},()->{intakeMotor.set(0.0);}));
+        controller.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenHeld(new StartEndCommand(()->{intakeMotor.set(0.5);},()->{intakeMotor.set(0.0);}));
+        controller.getGamepadButton(GamepadKeys.Button.A).toggleWhenPressed(
+                new InstantCommand(()->{servo.setPosition(0.75);}),
+                new InstantCommand(()->{servo.setPosition(0);})
+        );
     }
 }
